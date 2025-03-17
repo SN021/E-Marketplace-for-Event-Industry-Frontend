cfb850935811974eb804cc727951055a738b9b80
Sanduni Navodya <124874279+SN021@users.noreply.github.com>
2025-03-17 23:45:43 +0530

    sign in/signup page base

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className= "">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div>
+        <SignedIn><UserButton/></SignedIn>
+        <SignedOut>
+          <SignInButton/> 
+          <SignUpButton/> 
+        </SignedOut>
+      </div>
       <h1>
         Hi baby ball
       </h1>
