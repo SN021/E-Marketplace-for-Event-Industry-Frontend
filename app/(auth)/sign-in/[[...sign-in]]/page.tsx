aba014f28e0da1dd094d0d3e71f4b1c4f4e41780
Sanduni Navodya <124874279+SN021@users.noreply.github.com>
2025-03-18 01:31:37 +0530

    background added to the signin/up pagers

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,17 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return(
-  <div className="flex items-center justify-center h-screen">
-  <SignIn />
-  </div>
-  )
+  return (
+    <div className="">
+      <div
+        className=" absolute bg-cover bg-center h-screen w-full"
+        style={{
+          backgroundImage: "url('./images/signin.png')",
+        }}
+      ></div>
+      <div className="flex items-center justify-end px-13 md:px-30 lg:px-30 h-screen">
+        <SignIn />
+      </div>
+    </div>
+  );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,17 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return(
-  <div className="flex items-center justify-center h-screen">
-  <SignUp />
-  </div>
-  )
+  return (
+    <div className="">
+      <div
+        className=" absolute bg-cover bg-center h-screen w-full"
+        style={{
+          backgroundImage: "url('./images/signup.png')",
+        }}
+      ></div>
+      <div className="flex items-center px-13 md:px-30 lg:px-30 h-screen">
+        <SignUp />
+      </div>
+    </div>
+  );
 }
