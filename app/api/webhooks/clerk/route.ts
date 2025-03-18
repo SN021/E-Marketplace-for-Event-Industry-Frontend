0d822853b0abd24ec86cbac0a463f6e449305ab2
Damitha Udara <124874279+SN021@users.noreply.github.com>
2025-03-19 01:10:07 +0530

    api test 6

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -4,7 +4,28 @@ import { WebhookEvent } from '@clerk/nextjs/server';
 
 export const runtime = 'nodejs';
 
-export async function POST() {
+export async function POST(req: Request) {
+  const SIGNING_SECRET = process.env.SIGNING_SECRET;
+  if (!SIGNING_SECRET) {
+    throw new Error('Error: SIGNING_SECRET not set');
+  }
 
-  return new Response('Webhook received', { status: 200 });
+  const wh = new Webhook(SIGNING_SECRET);
+
+  // ❌ Don't do: await headers()
+  // ✅ Do it synchronously
+  const h = headers();
+  const svixId = (await h).get('svix-id');
+  const svixTimestamp = (await h).get('svix-timestamp');
+  const svixSignature = (await h).get('svix-signature');
+
+  if (!svixId || !svixTimestamp || !svixSignature) {
+    return new Response('Missing Svix headers', { status: 400 });
+  }
+
+  // Read the request body as JSON
+  const payload = await req.json();
+
+
+  return new Response(payload, { status: 200 });
 }
