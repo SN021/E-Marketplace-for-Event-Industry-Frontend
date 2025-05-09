02115f4bfac9c9adaabd76449949d42c9695b2a4
Sanduni Navodya <124874279+SN021@users.noreply.github.com>
2025-05-10 02:59:44 +0530

    Update ServicesHistory.tsx

--- app/dashboard/vendor-dashboard/_components/ServicesHistory.tsx
+++ app/dashboard/vendor-dashboard/_components/ServicesHistory.tsx
@@ -42,44 +42,51 @@ export default function ServicesHistory() {
     );
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 mx-auto gap-6">
-      {services.map((service, id) => (
-        <div
-          key={id}
-          className="bg-white shadow rounded-lg p-4 flex flex-col w-full max-w-[320px] sm:max-w-full justify-between "
-        >
-          <div>
-            <Image
-              src={service.thumbnail_url || "/placeholder.jpg"}
-              alt={service.service_title}
-              width={400}
-              height={300}
-              className="rounded-md h-[200px] object-cover"
-            />
+    <div>
+      <h1 className="text-xl font-semibold mb-8">Vendor Services History</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 mx-auto gap-6">
+        {services.map((service, id) => (
+          <div
+            key={id}
+            className="bg-white shadow rounded-lg p-4 flex flex-col w-full max-w-[320px] sm:max-w-full justify-between "
+          >
+            <div>
+              <Image
+                src={service.thumbnail_url || "/placeholder.jpg"}
+                alt={service.service_title}
+                width={400}
+                height={300}
+                className="rounded-md h-[200px] object-cover"
+              />
 
-            <div className="mt-4">
-              <h2 className="text-lg font-semibold">{service.service_title}</h2>
-              <p className="text-gray-500">{service.category}</p>
+              <div className="mt-4">
+                <h2 className="text-lg font-semibold">
+                  {service.service_title}
+                </h2>
+                <p className="text-gray-500">{service.category}</p>
+              </div>
+            </div>
+            <div className="flex  gap-2 mt-4">
+              <Link href={`/dashboard/edit-service/${service.id}`}>
+                <Button size="sm">Edit</Button>
+              </Link>
+              <Button
+                size="sm"
+                variant="secondary"
+                className="bg-red-500 hover:bg-red-400 text-white"
+                onClick={async () => {
+                  await axios.delete(`/api/vendor/services/${service.id}`);
+                  setServices((prev) =>
+                    prev.filter((s) => s.id !== service.id)
+                  );
+                }}
+              >
+                Remove
+              </Button>
             </div>
           </div>
-          <div className="flex  gap-2 mt-4">
-            <Link href={`/dashboard/edit-service/${service.id}`}>
-              <Button size="sm">Edit</Button>
-            </Link>
-            <Button
-              size="sm"
-              variant="secondary"
-              className="bg-red-500 hover:bg-red-400 text-white"
-              onClick={async () => {
-                await axios.delete(`/api/vendor/services/${service.id}`);
-                setServices((prev) => prev.filter((s) => s.id !== service.id));
-              }}
-            >
-              Remove
-            </Button>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
